4ef6b762c519a485cd4383237463a2d12c14a89d
luisj <luis.j20000@gmail.com>
2026-07-14 18:13:06 -0600

    Revert "fix: ruta native-callback que no consume code en servidor"
    This reverts commit a9c3bf04041b09529e911fffbd8ed22f054bff3a.

--- frontend/app/auth/native-callback/page.tsx
+++
@@ -1,9 +0,0 @@
-export const dynamic = 'force-static'
-
-export default function NativeCallback() {
-  return (
-    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', paddingTop: '40vh' }}>
-      <p>Completando inicio de sesión...</p>
-    </div>
-  )
-}
